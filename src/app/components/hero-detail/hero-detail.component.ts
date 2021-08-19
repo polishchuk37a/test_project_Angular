6c54d8606cd31829062146ac161e2b671f4966ca
polishchuk37a <polishchuk.andrey2002@gmail.com>
2021-08-19 13:12:18 +0300

    made the 4th tutorial using routes

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../../hero';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero } from "../../hero";
+import { HeroService} from "../../hero.service";
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero;
+  hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id === null) {
+      throw new Error('No such Hero');
+    }
+
+
+    this.heroService.getHero(+id)
+      .subscribe(hero => this.hero = (hero as Hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
